fd634f2a409a5eb2affdc2b7c4c15b2b3dd753e0
rishabh.sarkar@vinsol.com <47008145+RishabhSarkar@users.noreply.github.com>
2021-07-16 17:08:00 +0530

    Task

--- Hash.rb
+++ Hash.rb
@@ -1,7 +1,13 @@
-string = gets.chomp()
-hash = {}
-Unique = string.split('').uniq
-Unique.each do |str|
-    hash[str] = string.count(str)
+class Hash
+    attr_reader :input
+    def initialize(input)
+        @input = input
+    end
+    def str_count
+        letter_list = Hash.new(0)
+        input.downcase.scan(/\w/) {|letter|  letter_list[letter]= input.count(letter)}
+        return letter_list
+    end
 end
-puts hash
+input = gets.chomp()
+puts Hash.new(input).str_count
